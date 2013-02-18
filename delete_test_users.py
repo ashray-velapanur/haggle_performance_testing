d54837f5e24c2bdf850e9a261814d3bcc5b6bb66
Ashray <ashray@b-eagles.com>
2013-02-18 17:09:26 +0530

    fixing more bugs

--- delete_test_users.py
+++ delete_test_users.py
@@ -8,12 +8,19 @@ client_secret = '52c2d82565745aa4f2e203baa3867eac'
 access_token = urllib.urlopen('https://graph.facebook.com/oauth/access_token?client_id=' + client_id + '&client_secret=' + client_secret + '&grant_type=client_credentials').read()
 url = 'https://graph.facebook.com/' + client_id + '/accounts/test-users?' + access_token
 
-def delete_users(url):
+ids = []
+count = 0
+
+def get_ids(url):
     response = json.loads(urllib.urlopen(url).read())
     for user in response['data']:
-        print user['id']
-        urllib.urlopen('https://graph.facebook.com/' + user['id'] + '?method=delete&' + access_token)
+        ids.append(user['id'])
     if 'paging' in response and 'next' in response['paging']:
-        delete_users(response['paging']['next'])
+        get_ids(response['paging']['next'])
         
-delete_users(url)
+get_ids(url)
+
+for id in ids:
+    print count
+    count += 1
+    urllib.urlopen('https://graph.facebook.com/' + id + '?method=delete&' + access_token)
